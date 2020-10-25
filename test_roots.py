982b7dd4149c0e920dc2f4bd7ade2ef7c1083d83
Yujie Cai <ycai@g.harvard.edu>
2020-10-25 22:38:19 +0800

    modified test

--- test_roots.py
+++ test_roots.py
@@ -1,31 +1,24 @@
+import pytest
 import roots
 
 def test_quadroots_result():
     assert roots.quad_roots(1.0, 1.0, -12.0) == ((3+0j), (-4+0j))
 
 def test_quadroots_types():
-    try:
+    with pytest.raises(TypeError):
         roots.quad_roots("", "green", "hi")
-    except TypeError as err:
-        assert(type(err) == TypeError)
 
 def test_quadroots_zerocoeff():
-    try:
+    with pytest.raises(ValueError):
         roots.quad_roots(a=0.0)
-    except ValueError as err:
-        assert(type(err) == ValueError)
 
 def test_linearoots_result():
     assert roots.linear_roots(2.0, -3.0) == 1.5
 
 def test_linearroots_types():
-    try:
+    with pytest.raises(TypeError):
         roots.linear_roots("ocean", 6.0)
-    except TypeError as err:
-        assert(type(err) == TypeError)
 
 def test_linearroots_zerocoeff():
-    try:
+    with pytest.raises(ValueError):
         roots.linear_roots(a=0.0)
-    except ValueError as err:
-        assert(type(err) == ValueError)
